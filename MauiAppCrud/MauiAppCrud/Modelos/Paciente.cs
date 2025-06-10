c7b766af5d9e91068ba1d128285730a9684b4da1
sameaxel332 <sameaxel33@gmail.com>
2025-06-09 19:11:12 -0600

    proyecto bonito

---
+++ MauiAppCrud/MauiAppCrud/Modelos/Paciente.cs
@@ -0,0 +1,96 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace MauiAppCrud.Modelos
+{
+    public class Paciente
+    {
+        [Key]
+        public int IdPaciente { get; set; } 
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public double Peso { get; set; } // en kilogramos
+        public double Estatura { get; set; } // en metros
+        public string Sexo { get; set; } // "Masculino" o "Femenino"
+        public string NivelActividad { get; set; }
+
+        // Sedentario, Ligero, Moderado, Activo, Muy activo
+
+
+        public double IndiceMasa
+        {
+            get
+            {
+                double estaturaMetros = Estatura / 100.0;
+                double imc = Peso / Math.Pow(estaturaMetros, 2);
+                return Math.Truncate(imc * 100) / 100;
+            }
+        }
+
+        public double PorcentajeGrasaCorporal
+        {
+            get
+            {
+                double pgc;
+
+                if (Sexo == "Masculino")
+                {
+                    pgc = 1.20 * IndiceMasa + 0.23 * Edad - 16.2;
+                }
+                else
+                {
+                    pgc = 1.20 * IndiceMasa + 0.23 * Edad - 5.4;
+                }
+
+                return Math.Truncate(pgc * 100) / 100;
+            }
+        }
+        public double GastoEnergeticoTotal
+        {
+            get
+            {
+                double tmb;
+
+                if (Sexo == "Masculino")
+                {
+                    tmb = 88.36 + (13.4 * Peso) + (4.8 * Estatura) - (5.7 * Edad);
+                }
+                else
+                {
+                    tmb = 447.6 + (9.2 * Peso) + (3.1 * Estatura) - (4.3 * Edad);
+                }
+
+                double factor;
+
+                switch (NivelActividad)
+                {
+                    case "Sedentario":
+                        factor = 1.2;
+                        break;
+                    case "Ligero":
+                        factor = 1.375;
+                        break;
+                    case "Moderado":
+                        factor = 1.55;
+                        break;
+                    case "Activo":
+                        factor = 1.725;
+                        break;
+                    case "Muy activo":
+                        factor = 1.9;
+                        break;
+                    default:
+                        factor = 1.2;
+                        break;
+                }
+
+                double resultado = tmb * factor;
+                return Math.Truncate(resultado * 100) / 100;
+            }
+        }
+
+    }
+}
---
+++ MauiAppCrud/MauiAppCrud/ViewModels/MainViewModel.cs
@@ -0,0 +1,130 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+
+using CommunityToolkit.Mvvm.Input;
+
+using Microsoft.EntityFrameworkCore;
+using MauiAppCrud.DataAccess;
+using MauiAppCrud.DTOs;
+using MauiAppCrud.Utilidades;
+using MauiAppCrud.Modelos;
+using CommunityToolkit.Mvvm.Messaging;
+using System.Collections.ObjectModel;
+using MauiAppCrud.Views;
+
+namespace MauiAppCrud.ViewModels
+{
+    public partial class MainViewModel : ObservableObject 
+    {
+
+
+        private readonly PacienteDbContext _dbContext;
+        [ObservableProperty]
+        private ObservableCollection<PacienteDTO> listaPaciente = new ObservableCollection<PacienteDTO>();
+
+
+        public MainViewModel(PacienteDbContext context)
+        {
+            _dbContext = context;
+            MainThread.BeginInvokeOnMainThread(new Action(async () => await Obtener()));
+
+            WeakReferenceMessenger.Default.Register<PacienteMensajeria>(this, (r, m) =>
+            {
+                PacienteMensajeRecibido(m.Value);
+            });
+
+        }
+
+
+        public async Task Obtener()
+        {
+            var lista = await _dbContext.Pacientes.ToListAsync();
+            if (lista.Any())
+            {
+                foreach(var item in lista)
+                {
+                    ListaPaciente.Add(new PacienteDTO
+                    {
+                        IdPaciente = item.IdPaciente,
+                        Nombre = item.Nombre,
+                        Apellido = item.Apellido,
+                        Edad = item.Edad,
+                        Peso = item.Peso,
+                        Estatura = item.Estatura,
+                        Sexo = item.Sexo,
+                        NivelActividad = item.NivelActividad,
+                        IndiceMasa = item.IndiceMasa,
+                        PorcentajeGrasaCorporal = item.PorcentajeGrasaCorporal,
+                        GastoEnergeticoTotal = item.GastoEnergeticoTotal
+                    });
+                }
+            }
+        }
+
+
+        private void PacienteMensajeRecibido(PacienteMensaje pacienteMensaje)
+        {
+            var pacientDto = pacienteMensaje.PacienteDtO;
+
+            if (pacienteMensaje.EsCrear)
+            {
+                ListaPaciente.Add (pacientDto);
+            }
+            else
+            {
+                var encontrado = ListaPaciente.
+                    First(p=>p.IdPaciente == pacientDto.IdPaciente);
+
+                encontrado.Nombre = pacientDto.Nombre;
+                encontrado.Apellido = pacientDto.Apellido;
+                encontrado.Edad = pacientDto.Edad;
+                encontrado.Peso = pacientDto.Peso;
+                encontrado.Estatura = pacientDto.Estatura;
+                encontrado.Sexo = pacientDto.Sexo;
+                encontrado.NivelActividad = pacientDto.NivelActividad;
+                encontrado.IndiceMasa = pacientDto.IndiceMasa;
+                encontrado.PorcentajeGrasaCorporal = pacientDto.PorcentajeGrasaCorporal;
+                encontrado.GastoEnergeticoTotal = pacientDto.PorcentajeGrasaCorporal;
+                
+                
+            }
+        }
+
+
+        [RelayCommand]
+
+        private async Task Crear()
+        {
+            var uri = $"{nameof(PacientePage)}?id=0";
+            await Shell.Current.GoToAsync(uri);
+        }
+
+
+        [RelayCommand]
+        private async Task Editar(PacienteDTO pacienteDto)
+            
+        {
+            var uri = $"{nameof(PacientePage)}?id={pacienteDto.IdPaciente}";
+            await Shell.Current.GoToAsync(uri);
+        }
+
+        [RelayCommand]
+
+        private async Task Eliminar(PacienteDTO pacienteDto)
+
+        {
+            bool answer = await Shell.Current.DisplayAlert("Confirmar", "¿Desea eliminar el paciente?", "Si", "No");
+
+            if (answer)
+            {
+                var encontrado = await _dbContext.Pacientes.FirstAsync(p => p.IdPaciente == pacienteDto.IdPaciente);
+                _dbContext.Pacientes.Remove(encontrado);
+                await _dbContext.SaveChangesAsync();
+                ListaPaciente.Remove(pacienteDto);
+            }
+        }
+
+
+
+    }
+}
